8e128ad74b5b459d558b18ae15511782af545fd7
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-07-22 13:55:07 +0530

    add Pattern-programming-basics

--- src/JSpider/Pattern/Q1_Square_1.java
+++ src/JSpider/Pattern/Q1_Square_1.java
@@ -2,14 +2,66 @@ package JSpider.Pattern;
 
 public class Q1_Square_1 {
     public static void main(String[] args) {
-        int rows = 5;
-        int cols = 5;
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+
+        int rows = 7;
+        int cols = 3;
         for (int i = 0; i < rows; i++) {  // For Rows
             for (int j = 0; j < cols; j++) {  // For Columns
                 System.out.print(" * ");
             }
-            System.out.println(); // For entering in next line
+            System.out.println(); // For entering next line
         }
+        System.out.println("--------------------------");
 
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("(" + i + "," + j + ") ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
