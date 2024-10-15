64518a9d27687e7bca7b880da57f4b2a268ad537
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-10-16 00:20:04 +0530

    add String question

--- src/JSpider/String/PalindromeString.java
+++ src/JSpider/String/PalindromeString.java
@@ -10,11 +10,26 @@ public class PalindromeString {
     }
 
     public static boolean isPalindrome(String str) {
-        return reverseString(str).equals(str);
+        return reverseString(str).equalsIgnoreCase(str);
+    }
+
+    public static boolean isPalindrome2(String str) {
+        str = str.toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        String str = "KaaK";
+        String str = "KadddaK";
         System.out.println(isPalindrome(str));
+        System.out.println(isPalindrome2(str));
     }
 }
